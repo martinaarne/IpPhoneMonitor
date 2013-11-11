7e6f73134203634e4a79be6ef890d52606611ea4
Martin Aarne <martinaarne@gmail.com>
2013-11-11 14:34:50 +0200

    First commit

---
+++ IpPhoneMonitor/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace IpPhoneMonitor
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ IpPhoneMonitor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IpPhoneMonitor.MvcApplication" Language="C#" %>
